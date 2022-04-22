13a9808eb91bf6b9f777b99f0ccc62c2f6d46ad7
Wesley Ferreira <ovvesley@protonmail.com>
2022-04-22 18:21:20 +0000

    struct criado

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <threads.h>
 
+struct ptr {
+    int valor;
+    struct ptr *prox;
+}
+
+struct ptr *INICIO;
+
 int main(){
     printf("hello wolrd \n");
     printf("Tive acesso");
